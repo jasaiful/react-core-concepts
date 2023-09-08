9e579c1e6406262e4b9ad40188775c6481d197c2
jasaiful <jasaiful@gmail.com>
2023-09-08 19:49:36 +0600

    Explere event, state, useEffect

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './Counter'
+import Team from './Team'
+import Users from './Users'
+import Friends from './Friends'
+import Friend from './Friend'
+
+function App() {
+  function handleClick() {
+    alert('button clicked')
+  }
+
+  const handleClick2 = () => {
+    alert('button 2 clicked')
+  }
+
+    const addToFive = (num) => {
+      alert(num + 5);
+    }
+
+  return (
+    <>
+      <h3>React core concepts 2</h3>
+      
+            
+      <Friends></Friends>
+
+      <Users></Users>
+
+      <Team></Team>
+
+      <Counter></Counter>
+
+     <button onClick={handleClick} >Click Me</button>
+      <button onClick={handleClick2} >Click Me2</button>
+      <button onClick={() =>{alert ('third clicked')} } >Third Button</button>
+      {/* Jamela wala */}
+      <button onClick={() => addToFive(3)} >Four</button>
+    </>
+  )
+}
+
+export default App
